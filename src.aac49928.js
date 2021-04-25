86075ed6dafcba6e8b703f046838fa45e4b7bbd7
od-frontender <36302327+od-frontender@users.noreply.github.com>
2021-04-25 08:31:24 +0000

    Deploying to gh-pages from @ od-frontender/team-project@ca60eabd2dd9263e8646144f3b40c1f55eb7a47d 🚀

--- src.aac49928.js
+++ src.aac49928.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/reviews-brackets.png":[["reviews-brackets.7652c3c3.png","BQqq"],"BQqq"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project/src.2677fade.js.map
+//# sourceMappingURL=/team-project/src.aac49928.js.map
